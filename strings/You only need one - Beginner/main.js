96fc023fa85f23c768a701ddd637c599f12bb8a1
vazgen96 <vazgen90928@gmail.com>
2025-03-25 07:41:03 +0100

    string task done

---
+++ strings/You only need one - Beginner/main.js
@@ -0,0 +1,14 @@
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// a can contain numbers or strings. x can be either.
+
+// Return true if the array contains the value, false if not.
+
+function check(arr,x) {
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] === x) {
+            return true
+        }
+    }
+    return false
+}
